2b85eda87b985c289cc2396962567466d20f5794
Ivana Djuricic <iva.djuricic@gmail.com>
2025-05-14 17:42:28 +0200

    Added cart.py file to project + main.py modified with cart functionality

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
+import random
 
 def main_program ():
     p_manager = ProductManager()
@@ -21,3 +23,28 @@ def main_program ():
     
     print("Available Products:")
     p_manager.display_products()
+    
+    cart = Cart()
+    
+     # Nasumično biramo 3 proizvoda i dodajemo ih u korpu ako su dostupni
+    products = p_manager.products
+    for _ in range(3):
+        product = random.choice(products)  # Izaberi nasumičan proizvod
+        max_quantity = min(product.quantity, 3)  # Maksimum koji možemo da kupimo (ne više od 3)
+        if max_quantity > 0:
+            quantity = random.randint(1, max_quantity)  # Biramo količinu od 1 do max
+            cart.add_to_cart(product, quantity)  # Dodajemo u korpu
+
+    # Prikaz sadržaja korpe
+    print("\nCart Contents:")
+    cart.display_cart()
+
+    # Računanje ukupne vrednosti korpe
+    cart_total = cart.calculate_total()
+    print(f"\nTotal Cart Value: ${cart_total:.2f}")
+
+# Ova linija pokreće funkciju main() samo ako direktno pokrenemo fajl
+if __name__ == "__main__":
+    main_program()
+    
+    
